f421f27817018c0421948a2c21ef990db70abb39
Badr kamel <badrk.shokr@gmail.com>
2020-09-29 23:54:40 +0200

    Add API Testing

--- class-demos/flaskr/flaskr/__init__.py
+++ class-demos/flaskr/flaskr/__init__.py
@@ -16,6 +16,8 @@ def paginate_books(request, selection):
 
 	books = [book.format() for book in selection]
 	current_books = books[start:end]
+
+	return current_books
 #
 
 def create_app(test_config=None):
@@ -33,12 +35,27 @@ def create_app(test_config=None):
 
 	# Functions
 
+	@app.route("/books/search")
+	def search():
+		search = request.args.get('q', None, type=str)
+		if search:
+			result = Book.query.order_by(Book.rating).filter(Book.title.ilike(f'%{search}%'))
+			print(result)
+			return jsonify({
+					"success": True,
+					"result": [book.format() for book in result],
+				})
+		return jsonify({
+				"success": False,
+				"result": "no books found",
+			})
+	
 	@app.route("/books")	
-	def get_books():
+	def retrieve_books():
 
 		selection = Book.query.order_by(Book.id).all()
 		current_books = paginate_books(request, selection)
-
+		
 		if len(current_books) == 0:
 			return jsonify({
 					"message": "Nothing to show!"
@@ -46,8 +63,8 @@ def create_app(test_config=None):
 
 		return jsonify({
 				"success":True,
-				"books":formatted_books,
-				"total_books":len(books)
+				"books":current_books,
+				"total_books":len(selection)
 			})
 
 	@app.route("/books/<int:book_id>")
@@ -62,14 +79,18 @@ def create_app(test_config=None):
 	@app.route("/books/<int:book_id>/edit", methods=['PATCH'])
 	def update_book(book_id):
 
-		book = Book.query.get_or_404(book_id)
-		rating = request.get_json()['rating']
-		book.rating = rating
-		book.update()
+		try:
+			
+			book = Book.query.get_or_404(book_id)
+			rating = request.get_json()['rating']
+			book.rating = rating
+			book.update()
 
-		return jsonify({
-				'success':True
-			})
+			return jsonify({
+					'success':True
+				})
+		except:
+			abort(400)
 
 	@app.route("/books/<int:book_id>/delete", methods=['DELETE'])
 	def delete_book(book_id):
@@ -77,51 +98,50 @@ def create_app(test_config=None):
 		book = Book.query.get_or_404(book_id)
 		book.delete()
 
+		selection = Book.query.order_by(Book.rating).all()
+		current_books = paginate_books(request, selection)
+
 		return jsonify({
-				'success':True,
-				'deleted':book.id,
+				'success': True,
+				'deleted': book_id,
+				'books': current_books,
+				'total_books': len(selection)
 			})
 
 	@app.route("/books/create", methods=["POST"])
 	def create_book():
-		title = request.get_json()['title']
-		author = request.get_json()['author']
-		rating = request.get_json()['rating']
+		try:
+			title = request.get_json()['title']
+			author = request.get_json()['author']
+			rating = request.get_json()['rating']
 
-		book = Book(title=title, author=author, rating=rating)
-		book.insert()
+			book = Book(title=title, author=author, rating=rating)
+			book.insert()
 
-		return jsonify({
-				"success": True,
-				"created": book.id
-			})
+			return jsonify({
+					"success": True,
+					"created": book.id
+				})
+		except:
+			abort(422)
 	#
 
 	# Error handling
 
 	@app.errorhandler(404)
 	def not_found(error):
-		return jsonify({
-				"success": False,
-				"error": 404,
-				"message": "not found"
-			}), 404
+		return jsonify({"success": False,"error": 404,"message": "not found"}), 404
 
 	@app.errorhandler(422)
 	def unprocessable(error):
-		return jsonify({
-				"success": False,
-				"error": 422,
-				"message": "unprocessable"
-			}), 422
+		return jsonify({"success": False,"error": 422,"message": "unprocessable"}), 422
 
 	@app.errorhandler(405)
 	def not_allowed(error):
-		return jsonify({
-				"success": False,
-				"error": 405,
-				"message": "not allowed"
-			}), 405
-	#
+		return jsonify({"success": False,"error": 405,"message": "not allowed"}), 405
+
+	@app.errorhandler(400)
+	def bad_request(error):
+		return jsonify({"success": False,"error": 400,"message": "bad request"}), 400
 
 	return app
---
+++ class-demos/flaskr/test_flaskr.py
@@ -0,0 +1,110 @@
+import os
+import unittest
+import json
+from flask_sqlalchemy import SQLAlchemy
+
+from flaskr import create_app
+from flaskr.models import setup_db, Book
+
+class BookTestCase(unittest.TestCase):
+	"""This class represents the trivia test case"""
+
+	def setUp(self):
+		"""Define test variables and initialize app."""
+		self.app = create_app()
+		self.client = self.app.test_client
+		self.database_name = "bookshelf_test"
+		self.database_path = "postgresql://{}:{}@{}/{}".format('username', 'password','localhost:5432', self.database_name)
+		setup_db(self.app, self.database_path)
+
+		self.new_book = {
+			'title': 'Anansi Boys',
+			'author': 'Neil Gaiman',
+			'rating': 5
+		}
+
+		# binds the app to the current context
+		with self.app.app_context():
+			self.db = SQLAlchemy()
+			self.db.init_app(self.app)
+			# create all tables
+			self.db.create_all()
+	
+	def tearDown(self):
+		"""Executed after reach test"""
+		pass
+
+	def test_get_paginated_books(self):
+		res = self.client().get('/books')
+		data = json.loads(res.data)
+
+		self.assertEqual(res.status_code, 200)
+		self.assertEqual(data['success'], True)
+		self.assertTrue(data['total_books'])
+		self.assertTrue(len(data['books']))
+	
+	# @TODO: Write tests for search - at minimum two
+	#        that check a response when there are results and when there are none
+
+	def test_update_book_rating(self):
+		res = self.client().patch('/books/1/edit', json={'rating': 1})
+		data = json.loads(res.data)
+		book = Book.query.filter(Book.id == 1).one_or_none()
+
+		self.assertEqual(res.status_code, 200)
+		self.assertEqual(data['success'], True)
+		self.assertEqual(book.format()['rating'], 1)
+		
+
+	def test_400_for_failed_update(self):
+		res = self.client().patch('/books/1/edit')
+		data = json.loads(res.data)
+
+		self.assertEqual(res.status_code, 400)
+		self.assertEqual(data['success'], False)
+		self.assertEqual(data['message'], 'bad request')
+	
+	def test_delete_book(self):
+		res = self.client().delete('/books/2/delete')
+		data = json.loads(res.data)
+
+		book = Book.query.filter(Book.id == 2).one_or_none()
+
+		self.assertEqual(res.status_code, 200)
+		self.assertEqual(data['success'], True)
+		self.assertEqual(data['deleted'], 2)
+		self.assertTrue(data['total_books'])
+		self.assertTrue(len(data['books']))
+		self.assertEqual(book, None)
+		
+
+	def test_404_if_book_does_not_exist(self):
+		res = self.client().delete('/books/1000/delete')
+		data = json.loads(res.data)
+
+		self.assertEqual(res.status_code, 404)
+		self.assertEqual(data['success'], False)
+		self.assertEqual(data['message'], 'not found')
+	
+	def test_create_new_book(self):
+		res = self.client().post('/books/create', json=self.new_book)
+		data = json.loads(res.data)
+
+		self.assertEqual(res.status_code, 200)
+		self.assertEqual(data['success'], True)
+		self.assertTrue(data['created'])
+		self.assertTrue(len(data['books']))
+	
+	def test_405_if_book_creation_not_allowed(self):
+		res = self.client().get('/books/create', json=self.new_book)
+		data = json.loads(res.data)
+
+		self.assertEqual(res.status_code, 405)
+		self.assertEqual(data['success'], False)
+		self.assertEqual(data['message'], 'not allowed')
+	
+
+
+# Make the tests conveniently executable
+if __name__ == "__main__":
+	unittest.main()
